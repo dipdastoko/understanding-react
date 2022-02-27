d19aa82e4618b3249a1fcffd90e013fb0761d875
dipdastoko <dipdastoko@gmail.com>
2022-02-27 16:53:45 +0600

    unidirectional data flow

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Device from './components/Device/Device';
 
 function App() {
   return (
     <div className="App">
-
+      <Device name="phone" price="12000" />
     </div>
   );
 }
---
+++ src/components/Config/Config.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const config = (props) => {
+    return (
+        <div>
+            <ul>
+                <li>Price: {props.price}</li>
+            </ul>
+        </div>
+    );
+};
+
+export default config;
