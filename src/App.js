5bbd6613cc774af4df78237649b0c7192403404e
dipdastoko <dipdastoko@gmail.com>
2022-02-27 19:49:58 +0600

    asyncronous

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import Device from './components/Device/Device';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [steps, setSteps] = useState(0);
+
+  const handleIncreasedSteps = () => {
+    const newStepsCount = steps + 1;
+    setSteps(newStepsCount);
+    console.log(steps);
+  }
+  useEffect(() => {
+    console.log(steps);
+  }, [steps])
   return (
     <div className="App">
+      <h3>My Steps: {steps}</h3>
+      <button onClick={handleIncreasedSteps}>Walk</button>
       <Device name="phone" price="12000" />
     </div>
   );
